f45f5a1b5528d4a0aa41a851b36561607cde670f
wrbrown1990 <wrbrown@aggies.ncat.edu>
2019-10-17 20:40:50 -0400

    Classes and pointers

--- SeePlusPlus/SeePlusPlus.cpp
+++ SeePlusPlus/SeePlusPlus.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
+#include "SeePlusPlus.h"
 
 using namespace std;
+
+void memoryEffecientChange(int* age) {
+	*age = *age + 1;
+}
+
+class Triangle {
+	double height, base;
+public:
+	void setValues(double height, double base);
+	double area() {
+		return (height * base) / 2;
+	}
+};
+void Triangle::setValues(double x, double y) {
+	height = x;
+	base = y;
+}
 
 struct product { int weight; double price; };
 vector<product> list;
 int totalAppleWeight;
 
 void populateVector() {
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0; i < 10; i++) {
 		product apple;
 		apple.weight = rand() % 5 + 3;
 		apple.price = rand() % 10 + 0.5;
@@ -22,21 +40,35 @@ void printResults() {
 	for (int i = 0; i < list.size(); i++) {
 		cout << list.at(i).weight << " lb apple costs $" << list.at(i).price << endl;
 	}
-	cout << "Total weight = " << totalAppleWeight;
+	cout << "Total weight = " << totalAppleWeight << endl;
 }
 
 int main()
 {
-	//populateVector();
-	//printResults();
-	int dog = 10;
-	int* dogPointer = &dog;
-	dog = 15;
-	*dogPointer = 4;
-	cout << &dog << endl;//Returns dog's address in hexadecimal
-	cout << dogPointer << endl;
-	cout << dog << endl;
+	cout << "Vectors" << endl;
+	populateVector();
+	printResults();
+	cout << "Pointers" << endl;
+	pointers();
+	cout << "Classes" << endl;
+	classes();
 
+}
+
+void classes()
+{
+	Triangle triangle;
+	double height = 10;
+	double base = 7;
+	triangle.setValues(height, base);
+	cout << triangle.area() << endl;
+}
+
+void pointers()
+{
+	int age = 28;
+	memoryEffecientChange(&age);
+	cout << age << endl;
 }
 
 
---
+++ SeePlusPlus/SeePlusPlus.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void pointers();
+
+void classes();
